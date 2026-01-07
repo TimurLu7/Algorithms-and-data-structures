207d91feca810faddae52e09ec6c6059c66d7cf8
Timur <timurlu7@mail.ru>
2026-01-08 00:54:07 +0300

    graph algorithms

---
+++ Канонический вид 1/Канонический_вид.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");  
+
+    int n;
+    in >> n;
+
+    vector<int> arr(n + 1, 0);
+
+    for (int i = 1; i <= n-1; i++) {
+        int u, v;
+        in >> u >> v;
+        arr[v] = u;  
+    }
+
+    for (int i = 1; i <= n; i++) {
+        out << arr[i] << " ";
+    }
+
+    in.close();
+    out.close();
+    return 0;
+}
